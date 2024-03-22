82890a754de697404a44af3950615c079543431d
mohammed haydar <mohammedhaydar97@gmail.com>
2024-03-22 21:07:10 +0200

    added all ui fixes

--- lib/GlobalsController.dart
+++
@@ -1,9 +0,0 @@
-import 'package:get/get.dart';
-
-class GlobalController extends GetxController {
-  RxInt serviceIdState = 0.obs;
-
-  void updateServiceIdState(int newValue) {
-    serviceIdState.value = newValue;
-  }
-}
